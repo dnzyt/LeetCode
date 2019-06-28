be74473f3b5b256b479281786e93166b35fef8ee
dnzyt <dnzyt1986@gmail.com>
2019-06-27 23:43:38 -0400

    new change

---
+++ 23 Merge k Sorted Lists.js
@@ -0,0 +1,41 @@
+
+function ListNode(val) {
+    this.val = val
+    this.next = null;
+}
+
+var mergeKLists = function (lists) {
+    return merge(lists, 0, lists.length - 1)
+}
+
+var merge = function (lists, l, r) {
+    if (l > r) return null
+    if (l === r) return lists[l]
+    var mid = l + Math.floor((r - l) / 2)
+    return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r))
+
+}
+
+var mergeTwoLists = function (l1, l2) {
+
+    var dummy = new ListNode(0)
+    var p = dummy
+
+    while (l1 && l2) {
+
+        if (l1.val < l2.val) {
+            p.next = l1
+            l1 = l1.next
+            p = p.next
+        } else {
+            p.next = l2
+            l2 = l2.next
+            p = p.next
+        }
+
+    }
+    if (l1 === null) p.next = l2
+    if (l2 === null) p.next = l1
+
+    return dummy.next
+}
